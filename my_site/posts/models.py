3e55b74e8d987172ab32bb824330710afaa491cd
mista-ai <mr.bezdar@gmail.com>
2023-10-19 17:55:28 +0300

    book_app migration 1

--- my_site/posts/models.py
+++ my_site/posts/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.TextField()
-    content = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200, blank=True)
