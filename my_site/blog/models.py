80c051cfac1609318e708d9dec14332c999ed712
mista-ai <mr.bezdar@gmail.com>
2023-10-19 14:43:19 +0300

    corrected mistake, made it with posts

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-class Post(models.Model):
-    text = models.TextField()
-    content = models.CharField(max_length=199, blank=True)
--- my_site/posts/models.py
+++ my_site/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+    content = models.CharField(max_length=200, blank=True)
