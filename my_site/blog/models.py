c828b2c7bd3c6f2bf7e16281196210f7b24da5e5
mista-ai <mr.bezdar@gmail.com>
2023-10-19 14:31:04 +0300

    migrations 2

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.TextField()
-    title = models.CharField(max_length=200, blank=True)
+    content = models.CharField(max_length=199, blank=True)
