e2345e4332b38c0afac723b83e23809ca89ddeaf
mista-ai <mr.bezdar@gmail.com>
2023-10-20 07:50:44 +0300

    movie_proj and booshop database changes

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f'title = {self.title}, rating = {self.rating}'
