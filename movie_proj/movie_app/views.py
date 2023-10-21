3ecf3a5b1e56846ce5aae611393c52298fbea184
mista-ai <mr.bezdar@gmail.com>
2023-10-21 14:05:37 +0300

    slugified urls

--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -6,13 +6,16 @@ from .models import Movie
 
 def show_all_movies(request):
     movies = Movie.objects.all()
+    for movie in movies:
+        if movie.slug == '':
+            movie.save()
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
     })
 
 
-def show_one_movie(request, id_movie: int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
     })
