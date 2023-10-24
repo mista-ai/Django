54fd5f289b1722d3e1d7b3b0166f68b070bf9183
mista-ai <mr.bezdar@gmail.com>
2023-10-24 16:36:38 +0300

    get related objects

--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -58,9 +58,9 @@ class Movie(models.Model):
                                  validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True, related_name='movies')
 
-    actors = models.ManyToManyField(Actor)
+    actors = models.ManyToManyField(Actor, related_name='movies')
 
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.name)
