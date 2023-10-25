d3e66ac1ef02cde38876040a93657ca55eeef53c
mista-ai <mr.bezdar@gmail.com>
2023-10-25 17:53:41 +0300

    Class Based Views begin

--- form_project/feedback/urls.py
+++ form_project/feedback/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('done', views.done),
+    path('done', views.DoneView.as_view()),
+    path('', views.FeedBackView.as_view()),
+    path('<int:id_feedback>', views.FeedBackUpdateView.as_view()),
 ]
--- form_project/feedback/views.py
+++ form_project/feedback/views.py
@@ -3,18 +3,37 @@ from django.http.response import HttpResponseRedirect
 from django.http import HttpRequest
 from .forms import FeedbackForm
 from .models import Feedback
+from django.views import View
 
 
-def index(request: HttpRequest):
-    if request.method == 'POST':
+class FeedBackView(View):
+    def get(self, request):
+        form = FeedbackForm()
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+    def post(self, request):
         form = FeedbackForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/done')
-    else:
-        form = FeedbackForm()
-    return render(request, 'feedback/feedback.html', context={'form': form})
+        return render(request, 'feedback/feedback.html', context={'form': form})
 
 
-def done(request: HttpRequest):
-    return render(request, 'feedback/done.html')
+class FeedBackUpdateView(View):
+    def get(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(instance=feed)
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+    def post(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f'/{id_feedback}')
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+class DoneView(View):
+    def get(self, request):
+        return render(request, 'feedback/done.html')
