41b5c12627ce7a654dacdcd674b9c35c3aa5830a
mista-ai <mr.bezdar@gmail.com>
2023-10-25 08:03:30 +0300

    forms in django created

--- form_project/feedback/views.py
+++ form_project/feedback/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect
 from django.http import HttpRequest
+from .forms import FeebackForm
 
 
 # Create your views here.
 def index(request: HttpRequest):
     if request.method == 'POST':
-        name = request.POST['name']
-        if len(name) == 0 or len(name) > 20:
-            return render(request, 'feedback/feedback.html', context={'got_error': True})
-        print(name)
-        return HttpResponseRedirect('/done')
-    return render(request, 'feedback/feedback.html', context={'got_error': False})
+        form = FeebackForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/done')
+    form = FeebackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
+
 
 def done(request: HttpRequest):
     return render(request, 'feedback/done.html')
