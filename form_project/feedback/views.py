ef23702af9e72d14ef0f36e7f47208dc8395f72c
mista-ai <mr.bezdar@gmail.com>
2023-10-25 07:52:07 +0300

    manual validation of forms

--- form_project/feedback/views.py
+++ form_project/feedback/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.http import HttpRequest
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'feedback/feedback.html')
+def index(request: HttpRequest):
+    if request.method == 'POST':
+        name = request.POST['name']
+        if len(name) == 0 or len(name) > 20:
+            return render(request, 'feedback/feedback.html', context={'got_error': True})
+        print(name)
+        return HttpResponseRedirect('/done')
+    return render(request, 'feedback/feedback.html', context={'got_error': False})
+
+def done(request: HttpRequest):
+    return render(request, 'feedback/done.html')
