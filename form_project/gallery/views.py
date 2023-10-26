f11dc0f73994107ef7d510283b4fc290061a2d87
mista-ai <mr.bezdar@gmail.com>
2023-10-26 07:41:53 +0300

    File downloading

--- form_project/gallery/views.py
+++ form_project/gallery/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
+def storage_file(file):
+    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
+        for chunk in file.chunks():
+            new_file.write(chunk)
+
+
 class GalleryView(View):
     def get(self, request):
         return render(request, 'gallery/load_file.html')
 
     def post(self, request):
-        pass
+        storage_file(request.FILES['image'])
+        return render(request, 'gallery/load_file.html')
