80a361915d6fc153a4f597d437d17aa112b2a64f
mista-ai <mr.bezdar@gmail.com>
2023-10-27 13:59:03 +0300

    Django course done

--- form_project/gallery/urls.py
+++ form_project/gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('load_image', views.CreateGalleryView.as_view()),
+    path('list_image', views.ListGallery.as_view()),
 ]
--- form_project/gallery/views.py
+++ form_project/gallery/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import View, CreateView
+from django.views.generic import View, CreateView, ListView
 from .forms import GalleryUploadForm
 from django.http import HttpResponseRedirect
 from .models import Gallery
 
+
+class ListGallery(ListView):
+    model = Gallery
+    template_name = 'gallery/list_file.html'
+    context_object_name = 'records'
 
 
 class CreateGalleryView(CreateView):
@@ -11,7 +16,6 @@ class CreateGalleryView(CreateView):
     form_class = GalleryUploadForm
     template_name = 'gallery/load_file.html'
     success_url = '/load_image'
-
 
 # class GalleryView(View):
 #     def get(self, request):
